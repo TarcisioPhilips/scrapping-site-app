72af8e8b4b90563a5bc674137ed9187b550e2700
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-05-21 07:36:42 -0300

    feat: header component

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="bg-[#02A3FE]">
+
+      <nav
+        className='mx-auto flex max-ww7xl items-center justify-between p-6 lg:px-8'
+        aria-label="Global"
+      >
+        
+
+      </nav>
+
+    </header>
+  )
+}
